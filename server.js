b6eb0e68e8ddc4cc7afeb735a84b2157eff21f62
Rafael Uribe <jesus.uribe@oreillyauto.mx>
2026-05-27 16:12:17 -0600

    fix: use __dirname for dotenv path so server starts from any cwd

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-﻿require('dotenv').config();
+﻿require('dotenv').config({ path: require('path').join(__dirname, '.env') });
 require('./lib/config'); // validates required env vars, exits if missing
 
 const { startHttpServer, handleRequest } = require('./lib/http-server');
